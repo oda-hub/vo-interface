92319e8aa566d248797f2f761bbfb7f88270b378
burnout87 <gabriele.barni@gmail.com>
2024-10-28 17:51:41 +0100

    script to init the cluster

---
+++ setup_kind_cluster.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cat <<EOF > kind_cluster_config.yml
+kind: Cluster
+apiVersion: kind.x-k8s.io/v1alpha4
+nodes:
+  - role: control-plane
+    extraMounts:
+      - hostPath: ${DISPATCHER_CONFIG_DIR}
+        containerPath: /dispatcher/conf/conf.d
+    extraPortMappings:
+      - containerPort: 8001
+        hostPort: 8080
+EOF
+
+kind create cluster --config kind_cluster_config.yml
