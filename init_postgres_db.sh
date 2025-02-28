6645f53a589dbc2fb29cb3565ef7c04facb1e769
burnout87 <gabriele.barni@gmail.com>
2025-02-28 11:36:31 +0100

    less stuff in the init postgres script

--- init_postgres_db.sh
+++ init_postgres_db.sh
@@ -1,25 +1,6 @@
 #!/bin/bash
 set -e
 
-# Update and install necessary packages
-apt-get update && apt-get install -y vim \
-                                    build-essential \
-                                    software-properties-common \
-                                    git \
-                                    make \
-                                    gcc-9 \
-                                    postgresql-server-dev-16 \
-                                    bison \
-                                    byacc \
-                                    clang-13
-
-# Install the PostgreSQL extension for pgsphere
-git clone --branch aiprdbms16 https://github.com/kimakan/pgsphere.git /pgsphere
-
-cd /pgsphere && \
-    make USE_PGXS=1 PG_CONFIG=/usr/bin/pg_config && \
-    make USE_PGXS=1 PG_CONFIG=/usr/bin/pg_config install
-
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
 	CREATE DATABASE $POSTGRESQL_DB_NAME;
     \c $POSTGRESQL_DB_NAME;
@@ -27,11 +8,6 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
     SET search_path = $POSTGRESQL_DB_SCHEMA, public;
     CREATE EXTENSION pg_sphere;
 EOSQL
-
-cd /
-
-# get the pgloader tool and use it to load the data from the gallery DB onto the Postgres DB
-git clone https://github.com/dimitri/pgloader.git /pgloader
 
 cd /pgloader && \
     ./build/bin/pgloader mysql://$GALLERY_DB_USER:$GALLERY_DB_PASSWORD@$GALLERY_DB_HOST:$GALLERY_DB_PORT/$GALLERY_DB_NAME
