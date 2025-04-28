9dbc61fe55fd5e08adfccbbf5f536ce54e503e12
burnout87 <gabriele.barni@gmail.com>
2025-04-28 16:34:03 +0200

    using obscore

--- init_postgres_db.sh
+++ init_postgres_db.sh
@@ -18,31 +18,26 @@ cd /pgloader && \
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRESQL_USER" -d $POSTGRESQL_DB_NAME <<-EOSQL
     -- Create the view
-    CREATE VIEW $POSTGRESQL_DB_SCHEMA.data_product_table_view_v AS
+    CREATE VIEW $POSTGRESQL_DB_SCHEMA.obscore AS
     SELECT 
         node_field_data.title AS title,
         path_alias.alias AS path_alias,
         path_alias.path AS path,
-        node__field_product_id.field_product_id_value AS product_id,
         node__field_e1_kev.field_e1_kev_value AS e1_kev,
         node__field_e2_kev.field_e2_kev_value AS e2_kev,
         node__field_ra.field_ra_value AS ra,
         node__field_dec.field_dec_value AS dec,
         node__field_time_bin.field_time_bin_value AS time_bin,
         tax1.name AS instrument_name,
-        tax1.description__value AS instrument_description_value,
         tax2.name AS product_type_name,
-        tax2.description__value AS product_type_description_value,
         node__field_timerange.field_timerange_value AS timerange,
         node__field_timerange.field_timerange_end_value AS timerange_end,
         string_agg(node__field_obsid.field_obsid_value::text, ', ') AS proposal_id,
         string_agg(node__field_source_name.field_source_name_value::text, ', ') AS sources,
-        string_agg(SUBSTRING(f_m_1.uri FROM 10), ', ') AS file_uri,
+        string_agg(SUBSTRING(f_m_1.uri FROM 10), ', ') AS access_url,
         string_agg(SUBSTRING(f_m_2.uri FROM 10), ', ') AS image_uri
-    FROM
-        node_field_data
+    FROM node_field_data
         LEFT JOIN node__field_time_bin ON node_field_data.nid = node__field_time_bin.entity_id
-        LEFT JOIN node__field_product_id ON node_field_data.nid = node__field_product_id.entity_id
         LEFT JOIN node__field_e1_kev ON node_field_data.nid = node__field_e1_kev.entity_id
         LEFT JOIN node__field_e2_kev ON node_field_data.nid = node__field_e2_kev.entity_id
         LEFT JOIN node__field_ra ON node_field_data.nid = node__field_ra.entity_id
@@ -58,11 +53,11 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRESQL_USER" -d $POSTGRESQL_DB_NAME <<-
         LEFT JOIN node__field_describes_astro_entity ON node_field_data.nid = node__field_describes_astro_entity.entity_id
         LEFT JOIN node_field_data sources ON sources.nid = node__field_describes_astro_entity.field_describes_astro_entity_target_id
         LEFT JOIN node__field_source_name ON sources.nid = node__field_source_name.entity_id
-        LEFT JOIN path_alias ON path_alias.path::text LIKE ('/node/'::text || node_field_data.nid::text)
+        LEFT JOIN path_alias ON path_alias.path::text ~~ ('/node/'::text || node_field_data.nid::text)
         LEFT JOIN node__field_fits_file ON node_field_data.nid = node__field_fits_file.entity_id
-        LEFT JOIN file_managed AS f_m_1 ON node__field_fits_file.field_fits_file_target_id = f_m_1.fid
+        LEFT JOIN file_managed f_m_1 ON node__field_fits_file.field_fits_file_target_id = f_m_1.fid
         LEFT JOIN node__field_image_png ON node_field_data.nid = node__field_image_png.entity_id
-        LEFT JOIN file_managed AS f_m_2 ON node__field_image_png.field_image_png_target_id = f_m_2.fid
+        LEFT JOIN file_managed f_m_2 ON node__field_image_png.field_image_png_target_id = f_m_2.fid
     WHERE
         node_field_data.status = '1'::smallint
         AND node_field_data.type::text = 'data_product'::text
@@ -73,16 +68,13 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRESQL_USER" -d $POSTGRESQL_DB_NAME <<-
         node_field_data.title, 
         path_alias.alias, 
         path_alias.path, 
-        node__field_product_id.field_product_id_value, 
         node__field_e1_kev.field_e1_kev_value, 
         node__field_e2_kev.field_e2_kev_value, 
         node__field_ra.field_ra_value, 
         node__field_dec.field_dec_value, 
         node__field_time_bin.field_time_bin_value, 
         tax1.name, 
-        tax1.description__value, 
         tax2.name, 
-        tax2.description__value, 
         node__field_timerange.field_timerange_value, 
         node__field_timerange.field_timerange_end_value;
 EOSQL
